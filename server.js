b6b3834040f7bb6feaf9732c37b29a4bace05fe3
cristina <csuarezf09@gmail.com>
2024-04-02 14:45:26 -0600

    update server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
-app.listen(3000, () => console.log('Server started'))
+app.listen(process.env.PORT || 3000)
