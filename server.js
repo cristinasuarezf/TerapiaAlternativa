42604b4588c8f6ee07ac14a690138cbad9032fa4
cristina <csuarezf09@gmail.com>
2024-04-02 16:19:20 -0600

    Create design

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const especialistaRouter = require('./routes/especialistas')
@@ -16,10 +16,12 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
+const { init } = require('./models/paciente')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection
 db.on('error', error => console.error(error))
